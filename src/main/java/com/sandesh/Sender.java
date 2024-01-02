b4efae3c76273d16b4bfad67fc730b47568cf5f2
Aashish Katwal <aashiskutwal5@gmail.com>
2024-01-02 10:27:55 +0545

    Initial Commit

---
+++ src/main/java/com/sandesh/Sender.java
@@ -0,0 +1,12 @@
+package com.sandesh;
+
+public class Sender {
+	public void sendEmail(String to, String from, String subject, String message) {
+
+		// Gmail Host
+		String host = "smtp.gmail.com";
+
+//		Get System Properties
+
+	}
+}
